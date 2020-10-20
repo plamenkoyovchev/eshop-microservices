61f638e9ec4584552022743db577006af9a70014
Plamen Yovchev <plamenkooo@gmail.com>
2020-10-20 12:49:41 +0300

    Catalog controller initial commit

---
+++ src/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.API.Services.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductService productService;
+
+        public CatalogController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public async Task<IActionResult> Get()
+        {
+            var products = await this.productService.GetProductsAsync();
+
+            return Ok(products);
+        }
+    }
+}
